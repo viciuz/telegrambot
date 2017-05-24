fc1afc0d4da55445fd95980e20c776b638cf0dcb
viciuz <ziuvic@gmail.com>
2017-05-24 17:15:23 -0500

    telegram bot

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -7,27 +7,43 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		// Initialize Api Context
+		ApiContextInitializer.init();
 
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
+		// Instantiate Telegram Bots API
+		TelegramBotsApi botsApi = new TelegramBotsApi();
 
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
+		// Register our bot
+		try {
+			botsApi.registerBot(new MyAmazingBot());
+		} catch (TelegramApiException e) {
+			e.printStackTrace();
+		}
 
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
+	}
 
-        };
-    }
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
+	}
 
 }
